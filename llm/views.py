2112112564f3c7519e5575d2ab5ee50d93b08f52
Duguma Yeshitla (Ubuntu) <dugyeshi@gmail.com>
2024-08-27 15:22:48 +0900

    FEAT: implemented home page

--- llm/views.py
+++ llm/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def hello(request):
-    return render(request, "base.html")
+    return render(request, "home.html")
+
+def scout(request):
+    return render(request, "scout.html")
